33643ee1667f81a9593e8b211b1e46c4e4a21b23
phuuum <denphum.a@osngroups.com>
2025-04-03 17:27:37 +0700

    add: simple backend

---
+++ backend/app/api/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, APIRouter
+from app.mcp_tools import mcp as mcp_server
+from app.models import ChatRequest
+from app.mcp_client import MCPClient
+from fastapi.middleware.cors import CORSMiddleware
+
+
+mcp_client = MCPClient("http://localhost:8000/sse")
+
+
+app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+router = APIRouter(prefix="/chat", tags=["chat"])
+
+
+@router.get("/")
+def root():
+    return {"result": "success"}
+
+
+@router.post("/responses")
+async def responses(chat_request: ChatRequest):
+    response = await mcp_client.process_query(chat_request.message)
+    return {"result": response}
+
+
+app.include_router(router)
+app.mount("/", app=mcp_server.sse_app())
